f56075875aecd99ee9ceca5829839362f0333546
kiran.imandi.ext <kiran.imandi.ext@flydubai.com>
2020-09-23 12:37:36 +0530

    added home page, menu items

---
+++ src/components/directory/directory.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import MenuItem from '../menu-item/menu-item';
+import './directory.scss';
+
+class Directory extends React.Component {
+    state = { 
+        sections: [
+            {
+              title: 'hats',
+              imageUrl: 'https://images.unsplash.com/photo-1458546450666-ebb1e605853f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=748&q=80',
+              id: 1
+            },
+            {
+              title: 'jackets',
+              imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+              id: 2
+            },
+            {
+              title: 'sneakers',
+              imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+              id: 3
+            },
+            {
+              title: 'womens',
+              imageUrl: 'https://i.ibb.co/dgcsLhT/photo-1483985988355-763728e1935b.jpg',
+              size: 'large',
+              id: 4
+            },
+            {
+              title: 'mens',
+              imageUrl: 'https://images.unsplash.com/photo-1446214814726-e6074845b4ce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=461&q=80',
+              size: 'large',
+              id: 5
+            }
+          ]
+    };
+     
+    render() { 
+        
+        return ( 
+            <div className="directory-menu">
+                {
+                    this.state.sections.map(({id ,...props}) => (
+                        <MenuItem key={id} {...props}  />
+                    ))
+                }
+            </div>
+         );
+    }
+}
+ 
+
+export default Directory;
---
+++ src/pages/Homepage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Directory from '../components/directory/directory';
+
+const HomePage = () => {
+    return ( 
+        <div className="ui stackable grid container">
+            <Directory />
+        </div>
+     );
+}
+ 
+export default HomePage;
